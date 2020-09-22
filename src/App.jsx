352077cc249fc3ad00a5db494a4f301fbcf98e28
anselsmolund <smolu006@umn.edu>
2020-09-22 14:39:52 -0500

    fixing routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Admin from "./pages/Admin";
 import Contact from "./pages/Contact";
 import Video from "./pages/Video";
 import BlogPost from "./components/BlogPost";
-import { Switch, Route, NavLink, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserProvider from "./providers/UserProvider";
 import { motion, AnimatePresence } from "framer-motion";
 
@@ -17,22 +17,25 @@ const MainContainer = styled.div`
   font-family: "Dosis", sans-serif;
 `;
 function App() {
-  const location = useLocation();
   return (
-    <MainContainer>
-      <NavComponent />
-      <AnimatePresence>
-        <Switch location={location} key={window.location.pathname}>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/videos" component={Video} />
-          <Route exact path="/blog/:id" component={BlogPost} />
-          <Route exact path="/admin" component={Admin} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      </AnimatePresence>
-    </MainContainer>
+    <Router>
+      <UserProvider>
+        <MainContainer>
+          <NavComponent />
+          <AnimatePresence>
+            <Switch location={window.location} key={window.location.pathname}>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route exact path="/blog" component={Blog} />
+              <Route exact path="/videos" component={Video} />
+              <Route exact path="/blog/:id" component={BlogPost} />
+              <Route exact path="/admin" component={Admin} />
+              <Route exact path="/contact" component={Contact} />
+            </Switch>
+          </AnimatePresence>
+        </MainContainer>
+      </UserProvider>
+    </Router>
   );
 }
 
