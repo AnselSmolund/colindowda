be8e9604503f3bbf2579973e463284e4a1bcb3ff
anselsmolund <smolu006@umn.edu>
2020-09-24 12:48:39 -0500

    testing cool about page

--- src/components/NavComponent/SocialNavGroup/index.jsx
+++ src/components/NavComponent/SocialNavGroup/index.jsx
@@ -12,19 +12,20 @@ import { FaInstagram, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 
 export const SocialNavs = styled(Nav)``;
 function SocialNavGroup(props) {
+  const { color, size } = props;
   return (
     <SocialNavs className="justify-content-center">
-      <SocialNav link={INSTAGRAM_LINK}>
-        <FaInstagram></FaInstagram>
+      <SocialNav link={INSTAGRAM_LINK} color={color}>
+        <FaInstagram style={{ fontSize: size }}></FaInstagram>
       </SocialNav>
-      <SocialNav link={TWITTER_LINK}>
-        <FaTwitter></FaTwitter>
+      <SocialNav link={TWITTER_LINK} color={color}>
+        <FaTwitter style={{ fontSize: size }}></FaTwitter>
       </SocialNav>
-      <SocialNav link={YOUTUBE_LINK}>
-        <FaYoutube></FaYoutube>
+      <SocialNav link={YOUTUBE_LINK} color={color}>
+        <FaYoutube style={{ fontSize: size }}></FaYoutube>
       </SocialNav>
-      <SocialNav link={FACEBOOK_LINK}>
-        <FaFacebook></FaFacebook>
+      <SocialNav link={FACEBOOK_LINK} color={color}>
+        <FaFacebook style={{ fontSize: size }}></FaFacebook>
       </SocialNav>
     </SocialNavs>
   );
