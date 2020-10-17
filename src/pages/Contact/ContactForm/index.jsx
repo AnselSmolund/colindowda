63040ce05c8db5318406197ca6f1260e31710101
anselsmolund <smolu006@umn.edu>
2020-10-17 11:10:14 -0500

    contact form

---
+++ src/pages/Contact/ContactForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactForm(props) {
+  return (
+    <div>
+      <h1> Form here </h1>
+    </div>
+  );
+}
+
+export default ContactForm;
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -4,11 +4,13 @@ import styled from "styled-components";
 import { phoneOnly } from "../../util/breakpoints";
 import { MainTheme } from "../../styles/colors";
 import { MainContainer, Title } from "../../components/StyledComponents";
+import ContactForm from "./ContactForm";
 
 function Contact() {
   return (
     <MainContainer>
       <Title> Contact </Title>
+      <ContactForm></ContactForm>
     </MainContainer>
   );
 }
